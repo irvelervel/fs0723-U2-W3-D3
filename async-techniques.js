0bdfd81e8d12fceb20b817570a353949661b4d1a
Stefano Casasola <ste.casasola@gmail.com>
2024-01-17 10:05:45 +0100

    first commit

---
+++ async-techniques.js
@@ -0,0 +1,77 @@
+// ESEMPIO DI CODICE ASINCRONO
+
+const countUntilThree = function () {
+  setTimeout(() => {
+    console.log('ho contato fino a tre...')
+  }, Math.random() * 5000) // simulo un tempo variabile di conclusione timeout
+}
+
+const pageStart = function () {
+  countUntilThree() // questa riga non viene aspettata :(
+  console.log('FINITO!') // vorrei che questo console.log di FINITO arrivasse in fondo!
+}
+
+// pageStart()
+
+// riassumiamo il problema: avete due funzioni, la prima delle quali contiene un'operazione ASINCRONA (parte subito,
+// ma non sapete quando finisce :(...)
+// e avete la necessità di chiamare la seconda funzione solamente al termine della prima
+
+// SOLUZIONE 1) approccio con una CALLBACK
+// una callback è una funzione passata come parametro ad un'altra funzione
+
+const countUntilThreeWithCallback = function (nextCode) {
+  setTimeout(() => {
+    console.log('conto un intervallo variabile <5s')
+    nextCode()
+  }, Math.random() * 5000)
+}
+
+const pageStartWithCallback = function () {
+  countUntilThreeWithCallback(() => {
+    // questo è il CODICE SUCCESSIVO DA ESEGUIRE
+    console.log('FINITO!')
+  })
+}
+
+// pageStartWithCallback()
+
+// SOLUZIONE 2) approccio con una PROMISE
+// Una Promise è un wrapper, uno sugar coating sopra il concetto di callback
+// una Promise è un modo più elegante per scrivere e gestire un'operazione asincrona, e soprattutto
+// concaternare elegantemente diverse operazione asincrone in fila
+
+const countUntilThreeWithPromise = function () {
+  return new Promise((resolve, reject) => {
+    const timer = Math.random() * 5000
+
+    if (timer > 4000) {
+      // troppo!
+      reject()
+    } else {
+      setTimeout(() => {
+        console.log('conto un intervallo variabile <4s')
+        resolve()
+      }, timer)
+    }
+  })
+}
+
+const pageStartWithPromise = function () {
+  countUntilThreeWithPromise()
+    .then(
+      // qua inserisco il codice successivo da eseguire se l'operazione asincrona è finita BENE
+      // in pratica qua proseguo il flow una volta che nella Promise è stato chiamato resolve()
+      () => {
+        console.log('FINITO IN 4S O MENO')
+      }
+    )
+    .catch(() => {
+      // qua inserisco il codice successivo da eseguire se l'operazione asincrona è finita MALE
+      // in pratica qua proseguo il flow una volta che nella Promise è stato chiamato reject()
+
+      console.log('IL TIMER ERA TROPPO ALTO!')
+    })
+}
+
+pageStartWithPromise()
